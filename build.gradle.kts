685d59e0bc9a27855062c761b20ee819f4f14145
SayedAliElyas <alielyasrasool@gmail.com>
2026-04-18 15:47:20 +0200

    Refactor dependencies to use jbStarterVersion
    Updated dependencies to use jbStarterVersion for versioning.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,24 +4,24 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+val jbStarterVersion = "243.26574.91"
 
 repositories {
     mavenCentral()
     maven(url = "https://cache-redirector.jetbrains.com/intellij-dependencies")
     maven(url = "https://www.jetbrains.com/intellij-repository/releases")
-    maven(url = "https://www.jetbrains.com/intellij-repository/snapshots")
     maven(url = "https://download.jetbrains.com/teamcity-repository")
     maven(url = "https://cache-redirector.jetbrains.com/packages.jetbrains.team/maven/p/grazi/grazie-platform-public")
 }
 
 dependencies {
     // Starter + Driver (REQUIRED)
-    testImplementation("com.jetbrains.intellij.tools:ide-starter-squashed:LATEST-EAP-SNAPSHOT")
-    testImplementation("com.jetbrains.intellij.tools:ide-starter-junit5:LATEST-EAP-SNAPSHOT")
-    testImplementation("com.jetbrains.intellij.tools:ide-starter-driver:LATEST-EAP-SNAPSHOT")
-    testImplementation("com.jetbrains.intellij.driver:driver-client:LATEST-EAP-SNAPSHOT")
-    testImplementation("com.jetbrains.intellij.driver:driver-sdk:LATEST-EAP-SNAPSHOT")
-    testImplementation("com.jetbrains.intellij.driver:driver-model:LATEST-EAP-SNAPSHOT")
+    testImplementation("com.jetbrains.intellij.tools:ide-starter-squashed:$jbStarterVersion")
+    testImplementation("com.jetbrains.intellij.tools:ide-starter-junit5:$jbStarterVersion")
+    testImplementation("com.jetbrains.intellij.tools:ide-starter-driver:$jbStarterVersion")
+    testImplementation("com.jetbrains.intellij.driver:driver-client:$jbStarterVersion")
+    testImplementation("com.jetbrains.intellij.driver:driver-sdk:$jbStarterVersion")
+    testImplementation("com.jetbrains.intellij.driver:driver-model:$jbStarterVersion")
 
     // JUnit (REQUIRED)
     val junitBom = platform("org.junit:junit-bom:5.12.2")
